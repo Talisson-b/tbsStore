4a0e1608d693e9f025d0ebf34085f2f9239c1894
Talisson Barbosa <talissonbarbosa@gmail.com>
2023-10-17 21:43:27 -0300

    feat: add Categories and CategoriesItem component

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,12 +2,16 @@
 
 import { useSession } from "next-auth/react"
 import Image from "next/image"
+import Categories from "./components/categories"
 
 export default function Home() {
   
   return (
     <div className="p-5">
       <Image src='/banner-home-01.png' height={0} width={0} alt="até 55% de desconto esse mês" className="h-auto w-full" sizes="100vw"/>
+      <div className="mt-8">
+        <Categories />
+      </div>
     </div>
   )
 }
