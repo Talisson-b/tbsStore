e6e74ddfa72685e89581406f48be272ecc0cd44c
Talisson Barbosa <talissonbarbosa@gmail.com>
2023-10-18 00:29:45 -0300

    feat: edit component section title

--- src/app/(home)/components/section-title.tsx
+++ src/app/(home)/components/section-title.tsx
@@ -1,11 +1,11 @@
 import {ComponentProps } from "react"
 
 
-const SectionText = ({children, ...props}: ComponentProps<"p">) => {
+const SectionTitle = ({children, ...props}: ComponentProps<"p">) => {
   return (
     <p className="font-semibold uppercase pl-5 mb-3" {...props}>{children}</p>
    
   )
 }
 
-export default SectionText
+export default SectionTitle
